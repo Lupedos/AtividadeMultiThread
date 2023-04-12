1363797e4a44f1011805ec5cdbfd3b29950da183
Yasmimsantos6 <Yasmim_santos09@outlook.com>
2023-04-12 16:38:30 -0300

    CLASSE CORRIDA

--- src/Corrida.java
+++ src/Corrida.java
@@ -1,4 +1,22 @@
 
 public class Corrida {
-
+	public static void main(String[]args) {
+		System.out.println("Ready");
+		System.out.println("GO!");
+		car1 obj1 =new car1();
+		car2 obj2=new car2();
+		car3 obj3=new car3();
+		car4 obj4=new car4();
+		car5 obj5=new car5();
+		
+		
+		obj1.start();
+		obj2.start();
+		obj3.start();
+		obj4.start();
+		obj5.start();
+		
+		
+		//obj1.disp();
+		//obj2.disp();
 }
