69c2e2c65c8ed80d2391a7697cf143ea71db9b2f
mewzzie <amanda.duarte722@gmail.com>
2023-04-12 16:44:55 -0300

    ClassesCarro123

--- src/Corrida.java
+++ src/Corrida.java
@@ -1,4 +1,61 @@
+package teste;
+import java.util.Random;
 
+class car1 extends Thread{
+	int d =200 ;
+	int a ;
+	int i ;
+	Random random = new Random();
+	public void run() {
+		for(i=0;i<d;) {
+			 a = random.nextInt(50);
+			 i = i + a;
+			System.out.println("carro_1 andou:" + a +"metros"+ "    e já percorreu:"+i);
+			if(i>d){
+				System.out.println("chegou carro1 parabens"); }
+			
+			//try {Thread.sleep(1000);}catch(Exception e) {}//5 segundos
+			
+		}
+	}
+}
+
+class car2 extends Thread{
+	int d =200 ;
+	int a ;
+	int i ;
+	Random random = new Random();
+	public void run() {
+		for(i=0;i<d;) {
+			 a = random.nextInt(50);
+			 i = i + a;
+			System.out.println("carro_2 andou:" + a +"metros"+ "    e já percorreu:"+i);
+			if(i>d){
+				System.out.println("chegou carro2 parabens"); }
+			
+			//try {Thread.sleep(1000);}catch(Exception e) {}//5 segundos
+			
+		}
+	}
+}
+class car3 extends Thread{
+	int d =200 ;
+	int a ;
+	int i ;
+	Random random = new Random();
+	public void run() {
+		for(i=0;i<d;) {
+			 a = random.nextInt(50);
+			 i = i + a;
+			System.out.println("carro_3 andou:" + a +"metros"+ "    e já percorreu:"+i);
+			if(i>d){
+				System.out.println("chegou carro3 parabens"); }
+			
+			//try {Thread.sleep(1000);}catch(Exception e) {}//5 segundos
+			
+		}
+	}
+}
 public class Corrida {
 	public static void main(String[]args) {
 		System.out.println("Ready");
